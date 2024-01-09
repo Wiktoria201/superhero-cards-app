5021a2e563afad5df019b211cbcecaa4f226a11d
Wiktoria201 <139053443+Wiktoria201@users.noreply.github.com>
2024-01-09 20:08:55 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -32,3 +32,58 @@ export default App;
 // not displaying card with no properties/no image
 
 // animation after clicking on the card like heartstone
+
+/*
+function App() {
+  return (
+    <div>
+      <HerosesContextProvider>
+        <Search />
+        <Outlet />
+      <HerosesContextProvider>
+    </div>
+  );
+}
+*/
+
+/*
+w Search:
+  import {redirect} from 'react-router-dom'
+  const { addHero  } = useContext(HerosesContext)
+
+ const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const data = await searchByName(userInput);
+      const usersHero = data.results[0];
+
+      if (usersHero) {
+        isDuplicate = newHeroes.find((hero) => hero.id === usersHero.id);
+
+        if (!isDuplicate) {
+          -------  setNewHeroes([...newHeroes, usersHero]); // usuwamy z kodu
+          addHero(usersHero) // to dodajemy
+          redirect('/heroses-list') // przenosimy usera na komponent HerosesList
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    setUserInput("");
+  };
+
+*/
+
+/*
+w HerosesList:
+
+const {heroses, removeHero} = useContext(HerosesContext)
+
+<div>
+ {heroses.map(hero => <div>
+    <p>{hero.title}</p>
+    <button onClick={() => removeHero(hero.id)}>Usuń</button>
+  </div>)}
+</div>
+
+*/
