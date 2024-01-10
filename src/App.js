20864a614fef60bbeffe67d2236e7a8f7c23c102
Wiktoria201 <139053443+Wiktoria201@users.noreply.github.com>
2024-01-10 21:07:28 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -15,20 +15,8 @@ const App = () => {
 export default App;
 
 // to do:
-// -button generate random cards next to search that generates 3 cards again
-// -cards need to have a second side that shows the details of a character it needs to be done by react-router(cardsdetails)
-// -the chosen cards should be stored in an array in local storage, and have an icon that after clicking shows a list of cards img name total points
-
-// -each card when clicked needs to have a button accept decline
-// after accept -> adding a card into an array that will be stored
-// in another component, decline -> the card clicked should be removed and
-// it should fetch another card
-// -the array with chosen cards should have a max limit of 6 cards, they should be stored in local storage,
-// when clicked it should display mini images of the chosen cards on the left and the total points of each stat on the right
-// -after adding 6 cards the player will have an alert enemies, fight or run and abbandon/leave your heroes then the computer is math.random between the total numbers
-// deck component that is on 1 of the corners (stores all chosen cards
-
-// to fix:
-// not displaying card with no properties/no image
+// - not displaying cards without an image or stats
+// - if the user deletes all the cards display a message with a button / generate 3 new or search
+// - add the deck functionality
 
 // animation after clicking on the card like heartstone
