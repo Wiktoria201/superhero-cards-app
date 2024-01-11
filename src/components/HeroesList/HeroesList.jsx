a0e3e733954fadfe8462c3a6c2dfc23a5fb46de7
Wiktoria201 <139053443+Wiktoria201@users.noreply.github.com>
2024-01-11 20:09:50 +0100

    commit

--- src/components/HeroesList/HeroesList.jsx
+++ src/components/HeroesList/HeroesList.jsx
@@ -5,21 +5,37 @@ import { HeroesContext } from "../Context/HeroesContext";
 
 const HeroesList = () => {
   const { heroes, removeHero } = useContext(HeroesContext);
+
   return (
     <div className={styles.cardWrapper}>
       <div className={styles.newCardContainer}>
-        {heroes.map((hero) => (
-          <div>
-            <Card
-              key={hero.id}
-              id={hero.id}
-              name={hero.name}
-              powerstats={hero.powerstats}
-              image={hero.image}
-            />
-            <button onClick={() => removeHero(hero.id)}>Usuń</button>
+        {heroes.length === 0 ? (
+          <div className={styles.container}>
+            <p className={styles.text}>
+              You removed all of your cards. You can still search for new cards
+              or click on the button below to generate 3 random superheroes.
+            </p>
+            <button
+              className={styles.btn}
+              onClick={() => window.location.reload()}
+            >
+              Generate
+            </button>
           </div>
-        ))}
+        ) : (
+          heroes.map((hero) => (
+            <div>
+              <Card
+                key={hero.id}
+                id={hero.id}
+                name={hero.name}
+                powerstats={hero.powerstats}
+                image={hero.image}
+              />
+              <button onClick={() => removeHero(hero.id)}>Delete</button>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
