0e141c5b1608128136395f66a860ba1b2b9b089e
Wiktoria201 <139053443+Wiktoria201@users.noreply.github.com>
2024-01-16 22:11:44 +0100

    commit

--- src/components/HeroDetails/HeroDetails.jsx
+++ src/components/HeroDetails/HeroDetails.jsx
@@ -1,27 +1,3 @@
-// import styles from "./HeroDetails.module.css";
-// import { Link, useParams } from "react-router-dom";
-// import { useContext } from "react";
-// import { HeroesContext } from "../Context/HeroesContext";
-
-// const HeroDetails = () => {
-//   const { id } = useParams();
-//   const { heroes } = useContext(HeroesContext);
-//   const hero = heroes.find((hero) => hero.id === id);
-
-//   // redirect("/heroes-list");
-//   return (
-//     <div>
-//       <h1>{hero.name}</h1>
-//       <h1>Details for Hero {id}</h1>
-//       <Link to="/heroes-list">
-//         <button className={styles.btn}>Go Back</button>
-//       </Link>
-//     </div>
-//   );
-// };
-
-// export default HeroDetails;
-
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { useContext } from "react";
